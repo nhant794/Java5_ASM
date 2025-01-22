b0b3b238650fdcbf3e46b9298359bf59282d8d00
nhant794 <nhaxauzai05@gmail.com>
2025-01-22 19:23:29 +0700

    first commit

---
+++ src/main/java/org/example/java5_asm/Java5AsmApplication.java
@@ -0,0 +1,13 @@
+package org.example.java5_asm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java5AsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java5AsmApplication.class, args);
+    }
+
+}
