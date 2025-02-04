cd05f68fa966f7acb1b6aee5c95f82b8c0cb1983
nhant794 <nhaxauzai05@gmail.com>
2025-02-04 16:06:53 +0700

    4/2

---
+++ src/main/java/org/example/java5_asm/controller/customer/DetailProductController.java
@@ -0,0 +1,14 @@
+package org.example.java5_asm.controller.customer;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DetailProductController {
+
+    @GetMapping("/detail")
+    public String detailProduct() {
+        return "customer/detailedProduct";
+    }
+}
